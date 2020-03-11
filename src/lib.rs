6c2b5ce27f5973e33053094ba9ac20b2116f71e7
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2020-03-11 19:17:19 +0000

    Add deny and forbid

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,6 @@
+#![forbid(unsafe_code)]
+#![deny(unused_attributes, unused_imports, unused_mut, unused_must_use)]
+
 // As usual, we will use this file to carefully define the API/ what we expose to the user
 
 mod field;
