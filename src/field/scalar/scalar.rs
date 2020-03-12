af6de3809133d3459e911428cdcea098e78b4697
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2020-03-12 23:26:30 +0000

    add mul test for scalar

--- src/field/scalar/scalar.rs
+++ src/field/scalar/scalar.rs
@@ -85,11 +85,11 @@ impl Scalar {
     pub fn halve(&self) -> Self {
         let mut result = Scalar::zero();
 
-        let mask = -((self[0] & 1) as i64);
+        let mask = 0u32.wrapping_sub(self[0] & 1);
         let mut chain = 0u64;
 
         for i in 0..14 {
-            chain += (self[i] as u64) + ((MODULUS[i] as i64 & mask) as u64);
+            chain += (self[i] as u64) + ((MODULUS[i] & mask) as u64);
             result[i] = chain as u32;
             chain >>= 32
         }
@@ -216,6 +216,25 @@ fn test_basic_mul() {
     assert_eq!(ten * five, Scalar::from(50))
 }
 
+#[test]
+fn test_mul() {
+    let a = Scalar([
+        0xffb823a3, 0xc96a3c35, 0x7f8ed27d, 0x087b8fb9, 0x1d9ac30a, 0x74d65764, 0xc0be082e,
+        0xa8cb0ae8, 0xa8fa552b, 0x2aae8688, 0x2c3dc273, 0x47cf8cac, 0x3b089f07, 0x1e63e807,
+    ]);
+
+    let b = Scalar([
+        0xd8bedc42, 0x686eb329, 0xe416b899, 0x17aa6d9b, 0x1e30b38b, 0x188c6b1a, 0xd099595b,
+        0xbc343bcb, 0x1adaa0e7, 0x24e8d499, 0x8e59b308, 0x0a92de2d, 0xcae1cb68, 0x16c5450a,
+    ]);
+
+    let exp = Scalar([
+        0xa18d010a, 0x1f5b3197, 0x994c9c2b, 0x6abd26f5, 0x08a3a0e4, 0x36a14920, 0x74e9335f,
+        0x07bcd931, 0xf2d89c1e, 0xb9036ff6, 0x203d424b, 0xfccd61b3, 0x4ca389ed, 0x31e055c1,
+    ]);
+
+    assert_eq!(a * b, exp)
+}
 #[test]
 fn test_basic_square() {
     let five = Scalar::from(5);
