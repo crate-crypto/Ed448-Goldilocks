58418c94c5675eef18432b6ae528ecae1a9b7df7
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2020-04-12 14:44:57 +0100

    - Add constants file to curve module - Add curve module to library tree

---
+++ src/curve/constants.rs
@@ -0,0 +1,1 @@
+pub const EDWARDS_D: i64 = -39081;
--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,8 @@
 #![deny(unused_attributes, unused_imports, unused_mut, unused_must_use)]
 
 // As usual, we will use this file to carefully define the API/ what we expose to the user
-
-mod field;
+mod curve;
+pub mod field;
 #[cfg(test)]
 mod tests {
     #[test]
