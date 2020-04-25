118293c44d313781580e865b7b335fc3bb9dec9c
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2020-04-25 15:38:24 +0100

    cargo

--- src/curve/mod.rs
+++ src/curve/mod.rs
@@ -1,4 +1,6 @@
 pub(crate) mod constants;
 pub(crate) mod edwards;
+pub(crate) mod scalar_mul;
+pub(crate) mod twedwards;
 
 mod montgomery;
--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,14 @@
 #![forbid(unsafe_code)]
-#![deny(unused_attributes, unused_imports, unused_mut, unused_must_use)]
+// XXX: Change this to deny later on
+#![warn(unused_attributes, unused_imports, unused_mut, unused_must_use)]
 
 // As usual, we will use this file to carefully define the API/ what we expose to the user
 mod curve;
+pub mod decaf;
 pub mod field;
+pub mod ristretto;
 pub(crate) mod window;
+
 #[cfg(test)]
 mod tests {
     #[test]
