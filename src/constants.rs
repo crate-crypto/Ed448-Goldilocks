80c6d8ef5681a2b22fc73df9e3f48da4ac70b259
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2020-05-03 20:59:46 +0100

    Add test for generator through the 2-isogeny

--- src/constants.rs
+++ src/constants.rs
@@ -1,8 +1,18 @@
-use crate::curve::twedwards::extended::ExtendedPoint;
 use crate::decaf::DecafPoint;
-use crate::field::FieldElement;
 
 #[cfg(feature = "u32_backend")]
 pub(crate) use crate::field::u32::constants::*;
 
 pub(crate) const DECAF_BASEPOINT: DecafPoint = DecafPoint(TWISTED_EDWARDS_BASE_POINT);
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn test_base_point() {
+        let goldilocks_base = GOLDILOCKS_BASE_POINT;
+        // Use the isogeny to get the twisted edwards basepoint
+        let twisted_edwards_base = goldilocks_base.to_twisted();
+        assert!(twisted_edwards_base == TWISTED_EDWARDS_BASE_POINT)
+    }
+}
