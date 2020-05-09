8fcaa0305acea8c8c2fed25735c3418d312c8aa3
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2020-05-09 20:42:25 +0100

    clean up test and fix wrong import

--- src/curve/twedwards/projective.rs
+++ src/curve/twedwards/projective.rs
@@ -1,4 +1,4 @@
-use crate::curve::edwards::ExtendedPoint;
+use crate::curve::twedwards::extended::ExtendedPoint;
 use crate::field::FieldElement;
 use subtle::{Choice, ConditionallyNegatable, ConditionallySelectable};
 
@@ -58,45 +58,15 @@ impl ProjectiveNielsPoint {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use hex::decode as hex_decode;
-
-    fn slice_to_fixed_array(b: &[u8]) -> [u8; 56] {
-        let mut a: [u8; 56] = [0; 56];
-        a.copy_from_slice(&b);
-        a
-    }
-
-    fn hex_to_field(data: &str) -> FieldElement {
-        let mut bytes = hex_decode(data).unwrap();
-        bytes.reverse();
-        FieldElement::from_bytes(&slice_to_fixed_array(&bytes))
-    }
 
     #[test]
     fn test_conditional_negate() {
-        let Y_minus_X = hex_to_field("4b8a632c1feab72769cd96e7aaa577861871b3613945c802b89377e8b85331ecc0ffb1cb20169bfc9c27274d38b0d01e87a1d5d851770bc8");
-        let Y_plus_X = hex_to_field("81a45f02f41053f8d7d2a1f176a340529b33b7ee4d3fa84de384b750b35a54c315bf36c41d023ade226449916e668396589ea2145da09b95");
-        let Td = hex_to_field("5f5a2b06a2dbf7136f8dc979fd54d631ca7de50397250a196d3be2a721ab7cbaa92c545d9b15b5319e11b64bc031666049d8637e13838b3b");
-        let Z = FieldElement::one();
+        let bp = ExtendedPoint::generator();
 
-        let mut n = ProjectiveNielsPoint {
-            Y_plus_X,
-            Y_minus_X,
-            Td,
-            Z,
-        };
+        let mut bp_neg = bp.to_extensible().to_projective_niels();
+        bp_neg.conditional_negate(1.into());
 
-        let expected_neg_n = ProjectiveNielsPoint {
-            Y_plus_X: Y_minus_X,
-            Y_minus_X: Y_plus_X,
-            Td: Td.negate(),
-            Z: Z,
-        };
-
-        n.conditional_negate(1.into());
-
-        assert!(expected_neg_n.Y_plus_X == n.Y_plus_X);
-        assert!(expected_neg_n.Y_minus_X == n.Y_minus_X);
-        assert!(expected_neg_n.Td == n.Td);
+        let expect_identity = bp_neg.to_extended().add(&bp);
+        assert_eq!(ExtendedPoint::identity(), expect_identity);
     }
 }
