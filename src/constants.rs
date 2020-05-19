1d1d246461591f0ddab6d6bfd8bb68088931bcaa
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2020-05-19 21:25:43 +0100

    add fiat_u64 backend feature for constants

--- src/constants.rs
+++ src/constants.rs
@@ -3,16 +3,7 @@ use crate::decaf::DecafPoint;
 #[cfg(feature = "u32_backend")]
 pub(crate) use crate::field::u32::constants::*;
 
-pub(crate) const DECAF_BASEPOINT: DecafPoint = DecafPoint(TWISTED_EDWARDS_BASE_POINT);
+#[cfg(feature = "fiat_u64_backend")]
+pub(crate) use crate::field::fiat_u64::constants::*;
 
-#[cfg(test)]
-mod test {
-    use super::*;
-    #[test]
-    fn test_base_point() {
-        let goldilocks_base = GOLDILOCKS_BASE_POINT;
-        // Use the isogeny to get the twisted edwards basepoint
-        let twisted_edwards_base = goldilocks_base.to_twisted();
-        assert!(twisted_edwards_base == TWISTED_EDWARDS_BASE_POINT)
-    }
-}
+pub(crate) const DECAF_BASEPOINT: DecafPoint = DecafPoint(TWISTED_EDWARDS_BASE_POINT);
