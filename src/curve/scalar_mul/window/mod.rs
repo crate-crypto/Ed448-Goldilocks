77e81520c68113ff225f9f44ca599ca32526ab97
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2020-05-19 21:12:38 +0100

    remove select mask method

--- src/curve/scalar_mul/window/mod.rs
+++ src/curve/scalar_mul/window/mod.rs
@@ -1,7 +1,1 @@
 pub mod wnaf;
-
-// If they are the same return -1, if not return 0
-fn select_mask(index: u32, current: u32) -> u32 {
-    let equals = (index == current) as u32;
-    0u32.wrapping_sub(equals)
-}
