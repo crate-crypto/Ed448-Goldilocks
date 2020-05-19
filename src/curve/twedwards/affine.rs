169e28351c507206a1ce1e140c1be11e3b2d688e
Kevaundray Wedderburn <kevtheappdev@gmail.com>
2020-05-19 21:26:23 +0100

    - Remove to_extensible method from affine.

--- src/curve/twedwards/affine.rs
+++ src/curve/twedwards/affine.rs
@@ -133,49 +133,6 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_to_extensible() {
-        let a = AffinePoint {
-            x: FieldElement::from_raw_slice([
-                0x034365c8, 0x06b2a874, 0x0eb875d7, 0x0ae4c7a7, 0x0785df04, 0x09929351, 0x01fe8c3b,
-                0x0f2a0e5f, 0x0111d39c, 0x07ab52ba, 0x01df4552, 0x01d87566, 0x0f297be2, 0x027c090f,
-                0x0a81b155, 0x0d1a562b,
-            ]),
-            y: FieldElement::from_raw_slice([
-                0x00da9708, 0x0e7d583e, 0x0dbcc099, 0x0d2dad89, 0x05a49ce4, 0x01cb4ddc, 0x0928d395,
-                0x0098d91d, 0x0bff16ce, 0x06f02f9a, 0x0ce27cc1, 0x0dab5783, 0x0b553d94, 0x03251a0c,
-                0x064d70fb, 0x07fe3a2f,
-            ]),
-        };
-
-        let expected_b = ExtensiblePoint {
-            X: FieldElement::from_raw_slice([
-                0x034365c8, 0x06b2a874, 0x0eb875d7, 0x0ae4c7a7, 0x0785df04, 0x09929351, 0x01fe8c3b,
-                0x0f2a0e5f, 0x0111d39c, 0x07ab52ba, 0x01df4552, 0x01d87566, 0x0f297be2, 0x027c090f,
-                0x0a81b155, 0x0d1a562b,
-            ]),
-            Y: FieldElement::from_raw_slice([
-                0x00da9708, 0x0e7d583e, 0x0dbcc099, 0x0d2dad89, 0x05a49ce4, 0x01cb4ddc, 0x0928d395,
-                0x0098d91d, 0x0bff16ce, 0x06f02f9a, 0x0ce27cc1, 0x0dab5783, 0x0b553d94, 0x03251a0c,
-                0x064d70fb, 0x07fe3a2f,
-            ]),
-            Z: FieldElement::one(),
-            T1: FieldElement::from_raw_slice([
-                0x034365c8, 0x06b2a874, 0x0eb875d7, 0x0ae4c7a7, 0x0785df04, 0x09929351, 0x01fe8c3b,
-                0x0f2a0e5f, 0x0111d39c, 0x07ab52ba, 0x01df4552, 0x01d87566, 0x0f297be2, 0x027c090f,
-                0x0a81b155, 0x0d1a562b,
-            ]),
-            T2: FieldElement::from_raw_slice([
-                0x00da9708, 0x0e7d583e, 0x0dbcc099, 0x0d2dad89, 0x05a49ce4, 0x01cb4ddc, 0x0928d395,
-                0x0098d91d, 0x0bff16ce, 0x06f02f9a, 0x0ce27cc1, 0x0dab5783, 0x0b553d94, 0x03251a0c,
-                0x064d70fb, 0x07fe3a2f,
-            ]),
-        };
-
-        let b = a.to_extensible();
-        assert!(b == expected_b);
-    }
-
-    #[test]
     fn test_negation() {
         use crate::constants::TWISTED_EDWARDS_BASE_POINT;
         let a = TWISTED_EDWARDS_BASE_POINT.to_affine();
