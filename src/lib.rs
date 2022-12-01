54ffc6b8f14030688996da50a1bfb7bb713eb172
Conrado Gouvea <conradoplg@gmail.com>
2022-12-01 20:21:24 -0300

    add Scalar::from_canonical_bytes(), from_bytes_mod_order_wide() and random() (#23)

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,11 @@
 #![forbid(unsafe_code)]
 // XXX: Change this to deny later on
 #![warn(unused_attributes, unused_imports, unused_mut, unused_must_use)]
+
+// Internal macros. Must come first!
+#[macro_use]
+pub(crate) mod macros;
+
 // As usual, we will use this file to carefully define the API/ what we expose to the user
 pub mod constants;
 pub mod curve;
